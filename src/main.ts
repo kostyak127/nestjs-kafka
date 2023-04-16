1225a3a45c82be1e719f0fd537463d37322505bf
Коротченков Костя <kostyak127@gmail.com>
2023-04-16 13:02:40 +0500

    remove unused

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-
-require("dotenv").config();
-
-async function bootstrap() {
-  await NestFactory.create(AppModule);
-}
-bootstrap().then(() => console.log("bootstrapped"));

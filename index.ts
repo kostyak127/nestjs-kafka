8ab7008b4fa2ec034ea2abc16c8175a3a1136095
Коротченков Костя <kostyak127@gmail.com>
2023-06-12 13:04:14 +0500

    test index again

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./src/kafka/kafka.module";
-export * from "./src/kafka/kafka.decorators";
-export * from "./src/kafka/kafka.client";
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./kafka/kafka.module";
+export * from "./kafka/kafka.decorators";
+export * from "./kafka/kafka.client";
